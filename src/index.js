69c363e3f8091f49644f1867f8eb4edf71b5878d
Roman <rprikhoda@ukr.net>
2023-05-28 20:16:48 +0300

    add basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
